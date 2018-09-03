bc7435a65a914abfb2869267a7e6ad60a99c2d27
sam <your.email@here.com>
2018-09-02 19:30:58 -0700

    added everything

---
+++ constants.h
@@ -0,0 +1,187 @@
+
+/*
+ * projectDefines.h
+ *
+ *  Created on: Feb 11, 2017
+ *      Author: Sam2
+ */
+
+#ifndef PROJECTDEFINES_H_
+#define PROJECTDEFINES_H_
+
+#define ASSERT(x) do{ \
+	if(!x){while(1){}} \
+	}while(0)
+
+//********************************************************************************//
+//								SPI/ADC DEFINES									  
+//********************************************************************************//
+#define BLOCK_SPI_MEM_BYTE   							    256
+#define ADC_SAMPLES_PER_BLOCK							    128
+
+#define SPI_MEMORY_SRAM_SIZE_BYTES					  0x1FFFF
+#define SPI_TOTAL_SRAM_TOTAL_BLOCKS					  SPI_MEMORY_SRAM_SIZE_BYTES/BLOCK_SPI_MEM_BYTE
+
+#define SPI_MEMORY_BIT_RATE 					       	200000
+#define SPI_MEMORY_ADDRESS_SIZE     			  	8
+
+#define ADC_BIT_RATE 							 	 			  	180000
+#define ADC_BITS_PER_SAMPLE		    				  	16
+
+
+#define SPI_SRAM_SSI_BASE_INTERRUPT_BASE			INT_SSI0//INT_SSI0
+#define SPI_SRAM_SSI_BASE											0x40008000//SSI0_BASE
+#define SPI_ANALOG_SSI_BASE										0x40009000//SSI1_BASE
+
+
+#define MAXIMUM_MEMORY_ADRESS 								0x1FFFF //max range for 
+
+#define FIRST_BLOCK_SPI           		  	    0
+
+
+//********************************************************************************//
+//								SECTOR TRANSMIT CONSTANTS									  
+//********************************************************************************//
+
+
+#define NEW_TRASMITION_HEADER_B0	0xBA
+#define NEW_TRASMITION_HEADER_B1	0xDA
+#define NEW_TRASMITION_HEADER_B2	0xAD
+#define NEW_TRASMITION_HEADER_B3	0xBA
+
+#define TRASMITION_HEADER_B0			0xF0
+#define TRASMITION_HEADER_B1			0x0D
+#define TRASMITION_HEADER_B2			0xD0
+#define TRASMITION_HEADER_B3			0x00
+
+#define STOP_SEQUENCE_B0					0xD0
+#define STOP_SEQUENCE_B1					0x0D
+#define STOP_SEQUENCE_B2					0xD0
+#define STOP_SEQUENCE_B3					0x0D
+#define STOP_SEQUENCE_B4					0xD0
+
+
+#define FINAL_STOP_SEQUENCE_B0    0xDE
+#define FINAL_STOP_SEQUENCE_B1    0xAD
+#define FINAL_STOP_SEQUENCE_B2    0xDE
+#define FINAL_STOP_SEQUENCE_B3    0xAD
+#define FINAL_STOP_SEQUENCE_B4    0xDE
+
+
+#define TRASMIT_BLOCK_SIZE        (TRASMITION_HEADER_SIZE + TRASMIT_SECTOR_SIZE + SEQUENCE_ID_SIZE + CAP_VOLTAGE_SIZE + ESTIMATED_ENERGY_QUALITY_SIZE + DATA_SIZE + PGA_GAIN_SIZE + FREE_SIZE + CRC_SIZE + STOP_SEQUENCE_SIZE)
+
+//********************************************************************************//
+//								SECTOR TRANSMIT INDEX									  
+//********************************************************************************//
+
+#define NEW_TRASMITION_HEADER_B0_INDEX		 0
+#define NEW_TRASMITION_HEADER_B1_INDEX		 (NEW_TRASMITION_HEADER_B0_INDEX  + 1)
+#define NEW_TRASMITION_HEADER_B2_INDEX		 (NEW_TRASMITION_HEADER_B1_INDEX  + 1)
+#define NEW_TRASMITION_HEADER_B3_INDEX		 (NEW_TRASMITION_HEADER_B2_INDEX  + 1)
+
+
+#define TRASMITION_HEADER_B0_INDEX		 	   0
+#define TRASMITION_HEADER_B1_INDEX		 	   (TRASMITION_HEADER_B0_INDEX      + 1)
+#define TRASMITION_HEADER_B2_INDEX		 	   (TRASMITION_HEADER_B1_INDEX      + 1)
+#define TRASMITION_HEADER_B3_INDEX		 	   (TRASMITION_HEADER_B2_INDEX      + 1)
+
+
+#define TRASMIT_SECTOR_B0								   (NEW_TRASMITION_HEADER_B0_INDEX + NEW_TRASMITION_HEADER_SIZE)
+
+
+#define SEQUENCE_ID_B0_INDEX					     (TRASMIT_SECTOR_B0 + TRASMIT_SECTOR_SIZE)
+#define SEQUENCE_ID_B1_INDEX					     (SEQUENCE_ID_B0_INDEX +1)
+
+#define CAP_VOLTAGE_B0_INDEX						   (SEQUENCE_ID_B0_INDEX + SEQUENCE_ID_SIZE)
+#define CAP_VOLTAGE_B1_INDEX						   (CAP_VOLTAGE_B0_INDEX + 1)
+
+#define ESTIMATED_ENERGY_QUALITY_INDEX	   (CAP_VOLTAGE_B0_INDEX + CAP_VOLTAGE_SIZE)
+
+#define DATA_0_INDEX											 (ESTIMATED_ENERGY_QUALITY_INDEX + ESTIMATED_ENERGY_QUALITY_SIZE)
+
+#define PGA_GAIN_INDEX										 (DATA_0_INDEX + DATA_SIZE)
+
+#define FREE_B0_INDEX	  									 (PGA_GAIN_INDEX + PGA_GAIN_SIZE)
+#define FREE_B1_INDEX	  									 (FREE_B0_INDEX + 1)
+#define FREE_B2_INDEX	  									 (FREE_B1_INDEX + 1)
+#define FREE_B3_INDEX	  									 (FREE_B2_INDEX + 1)
+
+#define CRC_B0_INDEX										   (FREE_B0_INDEX + FREE_SIZE)
+#define CRC_B1_INDEX										   (CRC_B0_INDEX + 1)
+
+#define STOP_SEQUENCE_B0_INDEX			 		   (CRC_B0_INDEX + CRC_SIZE)
+#define STOP_SEQUENCE_B1_INDEX			 		   (STOP_SEQUENCE_B0_INDEX + 1)
+#define STOP_SEQUENCE_B2_INDEX			 		   (STOP_SEQUENCE_B1_INDEX + 1)
+#define STOP_SEQUENCE_B3_INDEX			 		   (STOP_SEQUENCE_B2_INDEX + 1)
+#define STOP_SEQUENCE_B4_INDEX			 		   (STOP_SEQUENCE_B3_INDEX + 1)
+
+
+#define FINAL_STOP_SEQUENCE_B0_INDEX       (CRC_B0_INDEX + CRC_SIZE)
+#define FINAL_STOP_SEQUENCE_B1_INDEX       (FINAL_STOP_SEQUENCE_B0_INDEX + 1)
+#define FINAL_STOP_SEQUENCE_B2_INDEX       (FINAL_STOP_SEQUENCE_B1_INDEX + 1)
+#define FINAL_STOP_SEQUENCE_B3_INDEX       (FINAL_STOP_SEQUENCE_B2_INDEX + 1)
+#define FINAL_STOP_SEQUENCE_B4_INDEX       (FINAL_STOP_SEQUENCE_B3_INDEX + 1)
+
+
+
+//********************************************************************************//
+//								SECTOR TRANSMIT SIZE									  
+//********************************************************************************//
+
+#define NEW_TRASMITION_HEADER_SIZE   		4
+
+#define TRASMITION_HEADER_SIZE		 	    4
+
+#define TRASMIT_SECTOR_SIZE             1
+
+#define SEQUENCE_ID_SIZE						   	2
+
+#define CAP_VOLTAGE_SIZE							  2
+
+#define ESTIMATED_ENERGY_QUALITY_SIZE		1
+
+#define DATA_SIZE												2048
+
+#define PGA_GAIN_SIZE										1
+
+#define FREE_SIZE												4
+
+#define CRC_SIZE											  2
+
+#define STOP_SEQUENCE_SIZE	   			 		5
+
+#define FINAL_STOP_SEQUENCE_SIZE	      5
+
+#define CRC_PROTECED_SIZE								(TRASMIT_SECTOR_SIZE + SEQUENCE_ID_SIZE + CAP_VOLTAGE_SIZE + ESTIMATED_ENERGY_QUALITY_SIZE + DATA_SIZE + PGA_GAIN_SIZE + FREE_SIZE) // size of data protected by CRC, starting at SEQUENCE_ID_B0_INDEX
+
+//********************************************************************************//
+//								blocks <---> sector logic							  
+//********************************************************************************//
+//currently there are 8 blocks per sector (2048/256)
+
+#define SECTORS_PER_SAMPLE	     3
+#define BLOCKS_PER_SECTOR	    (DATA_SIZE/BLOCK_SPI_MEM_BYTE) //8
+#define TOTAL_BLOCKS_IN_SAMPLE  (SECTORS_PER_SAMPLE*BLOCKS_PER_SECTOR)
+
+#define FIRST_TRASMIT_SECTOR						(0)
+#define LAST_TRANSMIT_SECTOR					  (SECTORS_PER_SAMPLE - 1)
+
+//********************************************************************************//
+//								VCAP targets before we kick off tasks(see notes.h)							  
+//********************************************************************************//
+
+#define VCAP_TARGET_SAMPLE   		   (0x164F)
+#define VCAP_TARGET_CREATE_TX_BUFF (0x164F)
+#define VCAP_TARGET_TRANSMIT 		   (0x1EAD)
+
+
+//********************************************************************************//
+//								build swithces									  
+//********************************************************************************//
+
+
+
+
+#define DEBUG_MODE					(1) 		//includes the halUnit test, creates buffer mmoery of them etc
+
+#endif /* PROJECTDEFINES_H_ */
---
+++ main.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "constants.h"
+
+FILE *ptr_myfile;
+FILE *ptr_myfile2;
+
+
+char temp[TRASMIT_BLOCK_SIZE*SECTORS_PER_SAMPLE];
+
+typedef enum {START_SECTOR, START_BLOCK, END_BLOCK, END_SECTOR, NO_MAGIC_KEY} magicKey_t;
+typedef enum {PROCESSING_START_BLOCK, PROCESSING_MIDDLE_BLOCK, PROCESSING_FINAL_BLOCK} currentBlock_t;
+
+typedef struct 
+{
+    uint8_t b0;
+    uint8_t b1;
+    uint8_t b2;
+    uint8_t b3;
+    uint8_t b4;
+}state_t;
+
+
+typedef struct 
+{
+    currentBlock_t   currentBlockState; //which block of our statemachine are we currently at (start, middle, end?)
+    uint32_t         uniqueId;      //the unique ID of a sector (will come in first block)
+    int              blockIndex;  //used to keep track of which byte of a single block we are curently parsing
+    state_t          shiftRegister; //will keep track of last 4 bytes which are shifted in
+    bool             processingSector; //will be true once we start processing a secotr, will be set to false when we are done or we hit an error condition
+    
+    bool             validBlock0;
+    bool             validBlock1;
+    bool             validBlock2;
+}validator_t;
+
+validator_t validator;
+
+
+
+   //uint64_t magicByte64  =  (uint64_t)0x10 << 32ull;
+   
+magicKey_t searchForMagicBytes()
+{
+    uint32_t magicByte32 =   validator.shiftRegister.b0 << 0u  |
+                             validator.shiftRegister.b1 << 8u  |
+                             validator.shiftRegister.b2 << 16u |
+                             validator.shiftRegister.b3 << 24u;
+
+    
+ 
+    uint64_t magicByte64  =  magicByte32 | (uint64_t)validator.shiftRegister.b4<< 32ull;
+    #if 0
+    printf("first byte is : %x ! \n", validator.shiftRegister.b0);
+    printf("second byte is: %x ! \n", validator.shiftRegister.b1);
+    printf("third byte is : %x ! \n", validator.shiftRegister.b2);
+    printf("fourth byte is: %x ! \n", validator.shiftRegister.b3);
+    printf("fifth byte is : %x ! \n", validator.shiftRegister.b4);
+
+    printf("\n");
+    printf("\n");
+    #endif 
+        
+   
+        
+    if(magicByte32 == 0xBADAADBA)
+    {
+        printf("Got a start sector magic number! \n");
+        return START_SECTOR;
+    }
+    else if(magicByte32 == 0xF00DD000)
+    {
+        printf("Got a start block magic number! \n");
+        return START_BLOCK;
+    }
+    
+    //end magic numbers are 5bytes (why?)
+    if(magicByte64 == 0xD00Dd00Dd0ull)
+    {
+        printf("Got an end block magic number! \n");
+        return END_BLOCK;
+    }
+    else if(magicByte64 == 0xDEADDEADDEull)
+    {
+        printf("Got a end sector magic number! \n");
+        return END_SECTOR;
+    }
+
+    return NO_MAGIC_KEY;
+    
+}
+
+
+
+//used to keep track of the last 5 byes we got
+static void shiftIn(char in)
+{
+   validator.shiftRegister.b4 = validator.shiftRegister.b3;
+   validator.shiftRegister.b3 = validator.shiftRegister.b2;
+   validator.shiftRegister.b2 = validator.shiftRegister.b1;
+   validator.shiftRegister.b1 = validator.shiftRegister.b0;
+   validator.shiftRegister.b0 = in;
+}
+
+static void processIncomingData(char in)
+{
+    shiftIn(in);
+    magicKey_t key = searchForMagicBytes();
+ 
+    
+    switch(validator.currentBlockState){
+    
+    case PROCESSING_START_BLOCK:
+        //if we get the magic number saying we will be transmitting a new sector, reset our states
+        if(START_SECTOR  == key)
+        {
+            validator.processingSector = true;
+            validator.blockIndex = 0;
+            break;
+        }
+        else if(END_BLOCK  == key)  
+        {
+            //got the first sector, go to next state 
+            printf("Processed the first block of a sector \n");
+            validator.currentBlockState = PROCESSING_MIDDLE_BLOCK;
+            validator.blockIndex = 0;
+            break;
+        }
+        else if(START_BLOCK  == key)  
+        {
+            //error, we should not be getting here - expect only END_BLOCK
+            validator.processingSector = false;
+            validator.blockIndex = 0;
+            break;
+        }
+        else if(END_SECTOR  == key)  
+        {
+            //error, we should not be getting here - expect only END_BLOCK
+            validator.processingSector = false;
+            validator.blockIndex = 0;
+            break;
+        }
+        else
+        {
+            //we did not hit a magic key
+            validator.blockIndex++;
+        }
+        if(validator.blockIndex > TRASMIT_BLOCK_SIZE)
+        {
+            //error, we should not be getting here - expect only END_BLOCK
+            validator.processingSector = false;
+            validator.blockIndex = 0;
+            break;
+        }
+   }
+}
+
+
+int main()
+{
+    ptr_myfile=fopen("user.c","rb");
+    fseek(ptr_myfile, 0, SEEK_END);
+    long fsize = ftell(ptr_myfile);
+    fseek(ptr_myfile, 0, SEEK_SET);  //same as rewind(f);
+    
+    
+    printf("size is %d \n", fsize);
+    
+    char *string = malloc(fsize + 1);
+    if(string == NULL)
+    {
+        printf("Failed to get buffer");
+    }
+    fread(string, fsize, 1, ptr_myfile);
+    fclose(ptr_myfile);
+    string[fsize] = 0;
+        printf("size is %d \n", fsize);
+    
+    int counter = 0;
+
+	ptr_myfile2=fopen("log.bin","wb");
+	if (!ptr_myfile)
+	{
+		printf("Unable to open file!");
+		return 1;
+	}
+    
+
+#if 0    
+    char fake [] = {0xDE , 0xAD, 0xDE, 0xAD, 0xDE};
+    
+    processIncomingData(fake[0]);
+
+
+
+    processIncomingData(fake[1]);
+
+    
+    
+    processIncomingData(fake[2]);
+
+    
+    
+    processIncomingData(fake[3]);
+
+    processIncomingData(fake[4]);
+
+    
+    return;
+ #endif
+ 
+    while(counter!=fsize)
+    {
+        
+        char tmp = (unsigned char)string[counter++];
+        processIncomingData(tmp);
+        printf("%d :  %1x \n",counter,(unsigned char)tmp);
+        fwrite (&tmp , sizeof(char), 1, ptr_myfile2);
+    }
+
+}
